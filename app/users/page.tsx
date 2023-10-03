c447421f4da82e94cdad70eef1534f04278c4bf2
Aaron Meads <aaron.meads@gmail.com>
2023-10-02 21:18:11 -0600

    Static rendering + dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,11 +5,14 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 10}})
+  // Revalidate = Makes that same api call every x I believe seconds
+  // const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 10}})
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
   const users: User[] = await res.json();
   return (
     <>
     <h1>Users</h1>
+    <p>{new Date().toLocaleString()}</p>
     <ul>
       {users.map(user => <li key={user.id}>{user.name}</li>)}
     </ul>
