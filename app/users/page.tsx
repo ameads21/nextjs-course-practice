1a1c323394ee708d628a417a80bd3c77f2a5521c
Aaron Meads <aaron.meads@gmail.com>
2023-10-02 21:12:03 -0600

    caching + api calls

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,20 @@
-const UsersPage = () => {
-  return <div>page</div>;
+interface User {
+  id: number;
+  name: string;
+  username: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 10}})
+  const users: User[] = await res.json();
+  return (
+    <>
+    <h1>Users</h1>
+    <ul>
+      {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+    </>
+  )
 };
 
 export default UsersPage;
