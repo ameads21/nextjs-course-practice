e3e410f9d53aba36f15c0ee26378b5057f73d250
Aaron Meads <aaronmeads@Aarons-MacBook-Pro-2.local>
2023-10-16 16:55:40 -0600

    Accessing query strings

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,3 +1,6 @@
+import { sort } from "fast-sort";
+import Link from "next/link";
+
 interface User {
   id: number;
   name: string;
@@ -5,23 +8,36 @@ interface User {
   email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
   // Revalidate = Makes that same api call every x I believe seconds
   // const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 10}})
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     cache: "no-store",
   });
   const users: User[] = await res.json();
+
+  const sortedUsers = sort(users).asc(
+    sortOrder === "email" ? (user) => user.email : (user) => user.name
+  );
+
   return (
     <table className="table table-bordered">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
+          <th>
+            <Link href="/users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
+        {sortedUsers.map((user) => (
           <tr key={user.id}>
             <td>{user.name}</td>
             <td>{user.email}</td>
