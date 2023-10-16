a850a838d45f366f882b4810e6375361be5f7861
Aaron Meads <aaronmeads@Aarons-MacBook-Pro-2.local>
2023-10-16 17:15:17 -0600

    Routing

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 const NewUserPage = () => {
-  return <div>NewUserPage</div>;
+  const router = useRouter();
+  return (
+    <button onClick={() => router.push("/users")} className="btn btn-primary">
+      Create
+    </button>
+  );
 };
 
 export default NewUserPage;
