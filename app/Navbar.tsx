434b2b46e5845dacad5c4c2268107eed90b2743f
Aaron Meads <aaronmeads@Aarons-MacBook-Pro-2.local>
2023-10-24 19:52:34 -0600

    Adding google authentication in our project

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex space-x-3 bg-slate-200 p-5">
       <Link href="/" className="mr-5">
         Next.js
       </Link>
       <Link href="/users">Users</Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
