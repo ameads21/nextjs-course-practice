7d4e5ee13619af9382b5111d5c10f7d0bfba356e
Aaron Meads <aaronmeads@Aarons-MacBook-Pro-2.local>
2023-10-24 20:02:28 -0600

    Server sessions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./Navbar";
+import AuthProvider from "./auth/Provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="winter">
       <body className={inter.className}>
-        <Navbar />
-        <main className="p-5">{children}</main>
+        <AuthProvider>
+          <Navbar />
+          <main className="p-5">{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-"use client";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import ProductCart from "./components/ProductCart";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCart />
     </main>
