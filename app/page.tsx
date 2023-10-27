878cd3b7b21e1e81366b1f6e8d7e705ed670f80a
Aaron Meads <aaronmeads@Aarons-MacBook-Pro-2.local>
2023-10-27 16:06:44 -0600

    Async imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,71 @@
-import { Metadata } from "next";
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import ProductCart from "./components/ProductCart";
+"use client";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+export default function Home() {
   return (
     <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCart />
+      <h1>Hello World</h1>
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default; // Use imports this way if the page is starting to bog down
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+
+          const sorted = _.orderBy(users, ["name"]);
+          console.log(sorted);
+        }}
+      >
+        Show
+      </button>
     </main>
   );
 }
 
-export async function generateMetadata(): Promise<Metadata> {
-  // const product = await fetch("");
+// "use client";
+// import dynamic from "next/dynamic";
+// import { useState } from "react";
+// import _ from 'lodash';
+// const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+//   ssr: false,
+//   loading: () => <p>Loading...</p>,
+// }); // Only use this method for large heavy components
 
-  return {
-    title: "product.title",
-    description: "...",
-  };
-}
+// export default function Home() {
+//   const [isVisible, setIsVisible] = useState(false);
+//   return (
+//     <main>
+//       <h1>Hello World</h1>
+//       <button onClick={() => setIsVisible(true)}>Show</button>
+//       {isVisible && <HeavyComponent />}
+//     </main>
+//   );
+// }
+
+// import { Metadata } from "next";
+// import { getServerSession } from "next-auth";
+// import Link from "next/link";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
+// import HeavyComponent from "./components/HeavyComponent";
+// import ProductCart from "./components/ProductCart";
+
+// export default async function Home() {
+//   const session = await getServerSession(authOptions);
+//   return (
+//     <main>
+//       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+//       <HeavyComponent />
+//       <Link href="/users">Users</Link>
+//       <ProductCart />
+//     </main>
+//   );
+// }
+
+// export async function generateMetadata(): Promise<Metadata> {
+//   // const product = await fetch("");
+
+//   return {
+//     title: "product.title",
+//     description: "...",
+//   };
+// }
 
 // import Image from "next/image";
 
